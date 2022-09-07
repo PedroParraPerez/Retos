8a39216c1a2a3babcadab607248132771b2bd793
PedroP <pedroparra06091994@gmail.com>
2022-09-07 02:18:26 +0200

    update folders

--- CICLO ANUAL CHINO/index.js
+++
@@ -1,4 +0,0 @@
-
-const elements = ['madera', 'fuego', 'tierra', 'metal', 'agua']
-const animals = ['rata', 'buey', 'tigre',
-    'conejo', 'dragón', 'serpiente', 'caballo', 'oveja', 'mono', 'gallo', 'perro', 'cerdo']
