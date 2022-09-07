978fe3bcda0e713641106e8b6e0b89544547b8f7
PedroP <pedroparra06091994@gmail.com>
2022-09-08 01:29:42 +0200

    reto 12

---
+++ Reto #12 ES UN PALINDROMO/app.js
@@ -0,0 +1,30 @@
+/*
+ * Reto #12
+ * ¿ES UN PALÍNDROMO?
+ * Fecha publicación enunciado: 21/03/22
+ * Fecha publicación resolución: 28/03/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba un texto y retorne verdadero o falso (Boolean) según sean o no palíndromos.
+ * Un Palíndromo es una palabra o expresión que es igual si se lee de izquierda a derecha que de derecha a izquierda.
+ * NO se tienen en cuenta los espacios, signos de puntuación y tildes.
+ * Ejemplo: Ana lleva al oso la avellana.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+const word = '?¿? ?aoto '
+
+const isPalindromo = (word) => {
+    const arr = word.toLowerCase().replace(/ /g, '').replace(/[.,\/#¿?!$%\^&\*;:{}=\-_`~()”“"…]/g, '').split('')
+    const rra = arr.slice().reverse();
+    if (arr.join() == rra.join()) return true
+    return false
+
+}
+console.log(isPalindromo(word))
